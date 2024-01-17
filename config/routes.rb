f53ed1e2d532c9eee63fc39d9578b5cbbbea5bb8
Bisaso Ronald <bronnie313@gmail.com>
2024-01-17 11:08:24 +0300

    Create The Events Record In The Application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :events
   resources :tickets
   root 'pages#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

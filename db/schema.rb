938f0a40df01312b4dedb9d8be0fe234e310049b
Bisaso Ronald <bronnie313@gmail.com>
2024-01-17 10:23:09 +0300

    Add Column Phone To The Tickets Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_09_192113) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_071902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_09_192113) do
     t.string "email_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
   end
 
 end

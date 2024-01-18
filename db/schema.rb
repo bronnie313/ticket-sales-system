d522c2e2582a6e4ce280b365a3ebf78d990cdb2b
Bisaso Ronald <bronnie313@gmail.com>
2024-01-18 09:57:59 +0300

    Customize View Labels

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_17_071902) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_080627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "artist"
+    t.text "description"
+    t.decimal "price_low"
+    t.decimal "price_high"
+    t.date "event_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tickets", force: :cascade do |t|
     t.string "name"
